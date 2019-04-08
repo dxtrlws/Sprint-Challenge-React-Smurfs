32101682a6c99c8a88be79af5a2414de16f3b67c
Dexter Lewis <dexter.lewis@gmail.com>
2019-04-07 21:21:06 -0400

    add routes to app.js

--- village/src/App.js
+++ village/src/App.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
-import Navbar from './components/Navbar'
-import axios from 'axios'
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
+import Navbar from "./components/Navbar";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+import { Route } from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
@@ -18,21 +20,28 @@ class App extends Component {
   // You'll need to make sure you have the right properties on state and pass them down to props.
 
   componentDidMount() {
-    axios.get('http://localhost:3333/smurfs')
-    .then(res => {
-      this.setState({ smurfs: res.data})
-    })
-    .catch(err => {
-      console.log(err);
-    })
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(res => {
+        this.setState({ smurfs: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
-  
+
   render() {
     return (
       <div className="App">
-      <Navbar />
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Navbar />
+        <Grid container style={{ marginTop: 40 }}>
+          <Route
+            exact
+            path="/"
+            render={props => <Smurfs {...props} smurfs={this.state.smurfs} />}
+          />
+          <Route path="/smurf-form" component={SmurfForm} />
+        </Grid>
       </div>
     );
   }
