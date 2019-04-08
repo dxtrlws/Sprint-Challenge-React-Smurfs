e9aac09a21f56312b1e62e693bc8739bb825ac99
Dexter Lewis <dexter.lewis@gmail.com>
2019-04-07 21:13:11 -0400

    Add navigation bar

--- village/src/components/Navbar.js
+++ village/src/components/Navbar.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { AppBar, Typography, Button, Toolbar } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+const linkStyle = {
+  color: "white",
+  textDecoration: "none"
+};
+const btnStyle = {
+  marginLeft: "20px"
+};
 
 const Navbar = () => {
-    return(
-        <h1>Hell from nav</h1>
-    )
-}
+  return (
+    <AppBar>
+      <Toolbar>
+        <Typography variant="h5" color="inherit">
+          <Link style={linkStyle} to="/">
+            Smurfs
+          </Link>
+        </Typography>
+        <Button style={btnStyle}>
+          <Link style={linkStyle} to="/smurf-form">
+            Add Smurf
+          </Link>
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-export default Navbar
+export default Navbar;
