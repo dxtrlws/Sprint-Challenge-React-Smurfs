7b50c6463eeb8d8fb6f4d6d3550b7915859f1885
Dexter Lewis <dexter.lewis@gmail.com>
2019-04-07 21:58:00 -0400

    Add delete functionality

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,20 +1,53 @@
-import React from 'react';
+import React from "react";
+import {
+  CardContent,
+  Card,
+  CardActions,
+  Typography,
+  IconButton,
+  Grid
+} from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import CreateIcon from "@material-ui/icons/Create";
+import axios from 'axios'
+
 
 const Smurf = props => {
+  const deleteHandler = () => {
+    axios.delete(`http://localhost:3333/smurfs/${props.id}`)
+    .then(res => {
+      props.updateSmurfs(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
   return (
-    <div className="Smurf">
+    <Grid item xs={12} md={6}>
+      <Card>
+        <CardContent>
       <h3>{props.name}</h3>
       <strong>{props.height} tall</strong>
       <p>{props.age} smurf years old</p>
-    </div>
+      <hr></hr>
+      </CardContent>
+      <CardActions>
+      <IconButton aria-label="Delete">
+            <CreateIcon color="primary" />
+          </IconButton>
+          <IconButton onClick={deleteHandler} aria-label="Delete">
+            <DeleteIcon  color="secondary" />
+          </IconButton>
+      </CardActions>
+      </Card>
+    </Grid>
   );
 };
 
 Smurf.defaultProps = {
-  name: '',
-  height: '',
-  age: ''
+  name: "",
+  height: "",
+  age: ""
 };
 
 export default Smurf;
-
--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
 import Smurf from './Smurf';
+import {Grid} from '@material-ui/core'
 
 class Smurfs extends Component {
   render() {
     return (
       <div className="Smurfs">
         <h1>Smurf Village</h1>
-        <ul>
+        <Grid container spacing={16} style={{width: 900}}>
           {this.props.smurfs.map(smurf => {
             return (
               <Smurf
@@ -20,7 +21,7 @@ class Smurfs extends Component {
               />
             );
           })}
-        </ul>
+        </Grid>
       </div>
     );
   }
